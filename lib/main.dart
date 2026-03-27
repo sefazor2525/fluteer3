5bfdf3edede70da7a4b1828c1c77c4b84a79721e
sefazor2525 <sefazor2525@gmail.com>
2026-03-27 15:10:36 +0300

    Update project changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +13,7 @@ class bugunneyesek extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.white,
         appBar: AppBar(
-          backgroundColor: Colors.lightGreenAccent,
+          backgroundColor: Colors.white,
           centerTitle: true,
           title: Text(
             "Bugün Ne Yesek ?",
@@ -24,10 +26,48 @@ class bugunneyesek extends StatelessWidget {
   }
 }
 
-class yemeksayfasi extends StatelessWidget {
+class yemeksayfasi extends StatefulWidget {
+  @override
+  State<yemeksayfasi> createState() => _yemeksayfasiState();
+}
+
+class _yemeksayfasiState extends State<yemeksayfasi> {
   int corbano = 1;
+
   int anayemekno = 1;
+
   int tatlino = 1;
+  List<String> corbaAdlari = [
+    "mercimek",
+    "tarhana",
+    "tavuk suyu",
+    "düğün çorbası",
+    "yoğurtlu çorba",
+  ];
+  List<String> anayemekadlari = [
+    "patlıcan",
+    "mantı",
+    "kuru fasülye",
+    "içli köfte",
+    "balık",
+  ];
+  List<String> tatliAdlari = [
+    "kadayıf",
+    "baklava",
+    "sütlaç",
+    "kazandibi",
+    "dondurma",
+  ];
+
+  void YemekDegistir() {
+    setState(() {
+      corbano = Random().nextInt(5) + 1;
+      anayemekno = Random().nextInt(5) + 1;
+      tatlino = Random().nextInt(5) + 1;
+    });
+    print("Yemekler Random Değiştirildi");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -36,27 +76,33 @@ class yemeksayfasi extends StatelessWidget {
           Expanded(
             child: TextButton(
               onPressed: () {
-                print("çorbaya tıklandı");
+                YemekDegistir();
               },
               child: Image.asset("assets/corba_$corbano.jpg"),
             ),
           ),
+          Text(corbaAdlari[corbano - 1], style: TextStyle(fontSize: 15)),
+          Divider(height: 10, color: Colors.red),
           Expanded(
             child: TextButton(
               onPressed: () {
-                print("ana yemeğe tıklandı");
+                YemekDegistir();
               },
               child: Image.asset("assets/yemek_$anayemekno.jpg"),
             ),
           ),
+          Text(anayemekadlari[anayemekno - 1], style: TextStyle(fontSize: 15)),
+          Divider(height: 10, color: Colors.red),
           Expanded(
             child: TextButton(
               onPressed: () {
-                print("tatlıya tıklandı");
+                YemekDegistir();
               },
               child: Image.asset("assets/tatli_$tatlino.jpg"),
             ),
           ),
+          Text(tatliAdlari[tatlino - 1], style: TextStyle(fontSize: 15)),
+          Divider(height: 10, color: Colors.red),
         ],
       ),
     );
